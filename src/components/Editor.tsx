7c0209b6d4944b5ec7268bd7521fb8a79e57afb9
Darren Burns <darrenb900@gmail.com>
2024-10-21 18:29:41 +0100

    Switch to tiptap

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,7 @@
-import React, { useRef, useEffect } from "react";
+import React, { useEffect, useState } from 'react';
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import { Extension } from '@tiptap/core';
 
 interface EditorProps {
   content: string;
@@ -8,53 +11,75 @@ interface EditorProps {
   highlightedKanji: Set<string>;
 }
 
-const Editor: React.FC<EditorProps> = ({ 
-  content, 
-  setContent, 
-  setSelectedText, 
-  fontSize, 
-  highlightedKanji 
+const HighlightKanji = Extension.create({
+  name: 'highlightKanji',
+  addGlobalAttributes() {
+    return [
+      {
+        types: ['textStyle'],
+        attributes: {
+          highlightedKanji: {
+            default: null,
+            parseHTML: element => element.getAttribute('data-highlighted-kanji'),
+            renderHTML: attributes => {
+              if (!attributes.highlightedKanji) {
+                return {};
+              }
+              return {
+                'data-highlighted-kanji': attributes.highlightedKanji,
+                class: 'highlighted-kanji',
+              };
+            },
+          },
+        },
+      },
+    ];
+  },
+});
+
+const Editor: React.FC<EditorProps> = ({
+  content,
+  setContent,
+  setSelectedText,
+  fontSize,
+  highlightedKanji,
 }) => {
-  const editorRef = useRef<HTMLDivElement>(null);
+  const [editorContent, setEditorContent] = useState(content);
 
-  const handleInput = () => {
-    if (editorRef.current) {
-      setContent(editorRef.current.innerText);
-    }
-  };
-
-  const handleSelect = () => {
-    const selection = window.getSelection();
-    if (selection) {
-      setSelectedText(selection.toString());
-    }
-  };
+  const editor = useEditor({
+    extensions: [StarterKit, HighlightKanji],
+    content: editorContent,
+    onUpdate: ({ editor }) => {
+      const newContent = editor.getText();
+      setContent(newContent);
+      setEditorContent(newContent);
+    },
+    onSelectionUpdate: ({ editor }) => {
+      setSelectedText(editor.state.selection.content().content.textBetween(0, -1, ' '));
+    },
+  });
 
   useEffect(() => {
-    if (editorRef.current) {
+    if (editor && editorContent !== content) {
       const kanjiRegex = /[\u4e00-\u9faf]/g;
-      const highlightedContent = content.split('\n').map(line => 
-        line.replace(kanjiRegex, (kanji) => {
-          if (highlightedKanji.has(kanji)) {
-            return `<span class="highlighted-kanji">${kanji}</span>`;
-          }
-          return kanji;
-        })
-      ).join('<br>');
-      editorRef.current.innerHTML = highlightedContent;
+      const highlightedContent = content.replace(kanjiRegex, (kanji) => {
+        if (highlightedKanji.has(kanji)) {
+          return `<span data-highlighted-kanji="true">${kanji}</span>`;
+        }
+        return kanji;
+      });
+      editor.commands.setContent(highlightedContent);
     }
-  }, [content, highlightedKanji]);
+  }, [content, highlightedKanji, editor]);
 
   return (
-    <div
-      ref={editorRef}
-      className="w-full h-full p-4 focus:outline-none border-r overflow-auto whitespace-pre-wrap"
-      style={{ fontSize: `${fontSize}px` }}
-      contentEditable
-      onInput={handleInput}
-      onSelect={handleSelect}
-      suppressContentEditableWarning={true}
-    />
+    <div className="editor-container w-full h-full overflow-auto">
+      <EditorContent
+        editor={editor}
+        className="editor-content h-full"
+        style={{ fontSize: `${fontSize}px` }}
+      />
+    </div>
   );
 };
 
